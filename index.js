55931c1a1c5dde6e9c95ddc2829e089717c18195
vratant11 <vratant2012061@akgec.ac.in>
2023-01-22 21:52:12 +0530

    Post router in progress

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import {register} from "./controllers/auth.js";
+import {createPost} from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATIONS
 
@@ -40,11 +44,14 @@ const upload = multer({ storage });
 
 //ROUTES WITH FILES
 
-app.post("/auth/register",upload.single("picture"), register);
+app.post("/auth/register",upload.single("picture"),register);
+app.post("/posts", verifyToken,upload.single("picture"), createPost);
 
 //ROUTES
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 //MONGOOSE SETUP
 mongoose.set("strictQuery", false);
